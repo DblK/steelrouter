77bd35ebb6df88d3cdcc280050a5fe0867fbf8bc
Rémy Boulanouar <r.boulanouar@zestia.fr>
2024-08-20 17:09:33 +0200

    fix: remove debug process

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'dblk:steel-router',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Iron router enforced for MeteorJS 3!',
   // URL to the Git repository containing the source code for this package.
